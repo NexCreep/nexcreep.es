99ac836cda1cee9361205cbd9c6e889abe874541
raulop194 <raulop194@alu.centroafuera.es>
2022-04-27 09:57:35 +0200

    dev: added color for distinct languages

--- src/core/Helpers/githubDelivery.js
+++ src/core/Helpers/githubDelivery.js
@@ -1,9 +1,5 @@
 export default async function getGithubUserRepos() {
-    const colorOfLang = {
-        javascript: "ffff00",
-        python: "3572a5"
-    }
-
+    const specialLangName = ["c#"]
     var formedHTML = [];
 
     try {
@@ -26,8 +22,8 @@ export default async function getGithubUserRepos() {
                 <div class="text-xl mt-2">${repo.description != null ? repo.description : "Amazing description 🌈"}</div>
                 <div class="mt-5">
                     <div class="inline-block align-middle dark:text-turq">
-                        <svg class="w-6 h-6 m-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
+                        <svg class="w-6 h-6">
+                            <circle cx="50%" cy="50%" r="8" class="lang-circle lang-circle-${!specialLangName.includes(repo.language.toLowerCase()) ? repo.language.toLowerCase() : "cs"}" />
                         </svg>
                     </div>
                     <div class="inline-block align-middle text-lg">${repo.language != null ? repo.language : "👻😱👻"}</div>
@@ -43,7 +39,6 @@ export default async function getGithubUserRepos() {
             </div>
         </a>
                 `
-
                 )
             }
         })
